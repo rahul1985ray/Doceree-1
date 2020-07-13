9cc7c7a1b7c32dfada4026632b3605e71ee49ca7
User <User@DESKTOP-1A7N0B8>
2020-07-13 18:41:16 +0530

    first commit

---
+++ Doceree_Test/src/test/java/advertiser_testcases/Audience.java
@@ -0,0 +1,50 @@
+package advertiser_testcases;
+
+import static org.testng.Assert.assertEquals;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class Audience {
+	
+	WebDriver driver = null;
+	
+	@BeforeMethod
+	public void set_up() {
+		String projectPath = System.getProperty("user.dir");
+		System.setProperty("webdriver.chrome.driver", projectPath + "\\Drivers\\Chrome\\chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().deleteAllCookies();
+		driver.manage().timeouts().implicitlyWait(50, TimeUnit.SECONDS);
+		driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
+	}
+	
+	@Test
+	public void createAudience() throws InterruptedException {
+		driver.get("https://qa-exchange.doceree.com/login");
+		driver.findElement(By.id("email")).sendKeys("rahul1985@yopmail.com");
+		driver.findElement(By.id("pwd")).sendKeys("Qwerty@2");
+		driver.findElement(By.xpath("//button[@type='submit']")).click();
+		Thread.sleep(10000);
+		driver.get("https://qa-exchange.doceree.com/advertiser/audience");
+		Thread.sleep(5000);
+		driver.findElement(By.xpath("(//img[@alt='audience'])[2]")).click();
+		driver.findElement(By.id("mat-input-0")).sendKeys("Hello");
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//button[contains(.,'Save')]")).click();
+		//assertEquals(driver.get, expected);
+	}
+	
+	@AfterMethod
+	public void teardown() {
+		//driver.quit();
+	}
+
+}
---
+++ Doceree_Test/src/test/java/advertiser_testcases/Login.java
@@ -0,0 +1,50 @@
+package advertiser_testcases;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertTrue;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Login {
+
+	WebDriver driver = null;
+
+	@BeforeMethod
+	public void set_up() {
+
+		String projectPath = System.getProperty("user.dir");
+	    System.setProperty("webdriver.chrome.driver", projectPath+"\\Drivers\\Chrome\\chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().deleteAllCookies();
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.manage().timeouts().pageLoadTimeout(50, TimeUnit.SECONDS);
+
+	}
+
+	@Test
+	public void advertiserlogin() {
+
+		driver.get("https://qa-exchange.doceree.com/login");
+		driver.findElement(By.id("email")).sendKeys("rahul1985@yopmail.com");
+
+		driver.findElement(By.id("pwd")).sendKeys("Qwerty@2");
+
+		driver.findElement(By.xpath("//button[@type='submit']")).click();
+		assertTrue(driver.findElement(By.linkText("Audience")).isDisplayed());
+
+	}
+
+	@AfterMethod
+	public void teardown() {
+		driver.quit();
+	}
+
+}
