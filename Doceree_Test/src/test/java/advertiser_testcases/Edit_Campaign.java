f2c036278bc409e04d47bf1ed15324dbcb52348c
User <User@DESKTOP-1A7N0B8>
2020-08-18 16:11:02 +0530

    Edit campaign test case completed

--- Doceree_Test/src/test/java/advertiser_testcases/Edit_Campaign.java
+++ Doceree_Test/src/test/java/advertiser_testcases/Edit_Campaign.java
@@ -11,7 +11,7 @@ public class Edit_Campaign extends TestBase {
 	public void editCampaign() throws InterruptedException {
 	driver.get(baseURL);
 	driver.findElement(By.id("email")).sendKeys("rahul1985@yopmail.com");
-	driver.findElement(By.id("pwd")).sendKeys("Qwerty@2");
+	driver.findElement(By.id("pwd")).sendKeys("Test@123");
 	driver.findElement(By.xpath("//button[@type='submit']")).click();
 	Thread.sleep(10000);
 	driver.findElement(By.linkText("Manage Campaigns")).click();
@@ -24,7 +24,27 @@ public class Edit_Campaign extends TestBase {
 	Thread.sleep(2000);
 	driver.findElement(By.xpath("//span[contains(text(),'Edit')]")).click();
 	Thread.sleep(2000);
-	driver.findElement(By.xpath("//input[@id='mat-input-7']")).sendKeys("Edit subcampaign");
+	driver.findElement(By.xpath("//input[@id='mat-input-9']")).clear();
+	Thread.sleep(1000);
+	driver.findElement(By.xpath("//input[@id='mat-input-9']")).sendKeys("Edit subcampaign");
+	Thread.sleep(1000);
+	driver.findElement(By.xpath("//button[contains(.,'Next')]")).click();
+	Thread.sleep(4000);
+	driver.findElement(By.xpath("(//button[contains(.,'Next')])[2]")).click();
+	Thread.sleep(2000);
+	driver.findElement(By.xpath("(//button[contains(.,'Next')])[3]")).click();
+	driver.findElement(By.id("mat-input-13")).clear();
+	driver.findElement(By.id("mat-input-13")).sendKeys("44");
+	Thread.sleep(1000);
+	driver.findElement(By.id("mat-input-14")).clear();
+	driver.findElement(By.id("mat-input-14")).sendKeys("2500");
+	Thread.sleep(1000);
+	driver.findElement(By.id("mat-input-15")).clear();
+	driver.findElement(By.id("mat-input-15")).sendKeys("88");
+	Thread.sleep(1000);
+	driver.findElement(By.xpath("(//button[contains(.,'Next')])[4]")).click();
+	Thread.sleep(2000);
+	driver.findElement(By.xpath("//button[contains(.,'Save & Publish')]")).click();
 	
 	}
 }
